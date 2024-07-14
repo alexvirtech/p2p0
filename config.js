1fbfe16a06a12b3c80e69d583d446cc8f8cb9e77
alexvirtech <alex@vir-tec.net>
2024-07-14 18:08:57 +0300

    140724

---
+++ config.js
@@ -0,0 +1,19 @@
+const peerConfig = {
+    //host: 'wss2.mtw-testnet.com',
+    //path: '/peerjs/myapp',
+    config: {
+        iceServers: [
+            { urls: "stun:stun.l.google.com:19302" },
+            {
+                urls: "turn:standard.relay.metered.ca:443",
+                username: "13bc22c6d7ffd548f62b9557",
+                credential: "vAOlbk9/uyITlI1m",
+            },
+            {
+                urls: "turns:standard.relay.metered.ca:443?transport=tcp",
+                username: "13bc22c6d7ffd548f62b9557",
+                credential: "vAOlbk9/uyITlI1m",
+            },
+        ],
+    },
+}

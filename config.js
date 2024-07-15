a6c170e84975f3ca3c8ea2303e84a5cdab6c9129
Alex Keselman <alex@vir-tec.net>
2024-07-15 14:12:53 +0300

    15-07-24

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 const peerConfig = {
     host: 'wss2.mtw-testnet.com',
+    path: '/peerjs/myapp'
+}
+
+const peerConfig1 = {
+    host: 'wss2.mtw-testnet.com',
     path: '/peerjs/myapp',
     config: {
         iceServers: [
